7ba2503cea18bd57f261f332b8471a4935ba580c
Johan <jottossonta@gmail.com>
2025-09-01 10:50:32 +0200

    Update rune-vcs to v0.3.2-alpha.6
    - Documentation overhaul with Raycast-inspired design
    - Brand update from 'Rune VCS' to 'Rune'
    - ARM64 macOS support only (Intel build issues with OpenSSL)
    - SHA256: 287ca9250b499f7aac37b1f866136e7663bd66e26b708bd751fa56363b114377
    
    Release notes: https://github.com/Johan-Ott/rune-vcs/releases/tag/v0.3.2-alpha.6

--- Formula/rune-vcs.rb
+++ Formula/rune-vcs.rb
@@ -1,15 +1,13 @@
 class RuneVcs < Formula
   desc "Modern, intelligent version control system"
   homepage "https://github.com/Johan-Ott/rune-vcs"
-  url "https://github.com/Johan-Ott/rune-vcs/releases/download/v0.3.0-alpha.4/rune-v0.3.0-alpha.4-x86_64-apple-darwin.tar.gz"
-  sha256 "087560cb92af12057c0dc7a653adf2d79f8c48f0b32b1151eea4b166410da0b2"
+  url "https://github.com/Johan-Ott/rune-vcs/releases/download/v0.3.2-alpha.6/rune-0.3.2-alpha.6-aarch64-apple-darwin.tar.gz"
+  sha256 "287ca9250b499f7aac37b1f866136e7663bd66e26b708bd751fa56363b114377"
   license "Apache-2.0"
-  version "0.3.0-alpha.4"
+  version "0.3.2-alpha.6"
 
-  on_arm do
-    url "https://github.com/Johan-Ott/rune-vcs/releases/download/v0.3.0-alpha.4/rune-v0.3.0-alpha.4-aarch64-apple-darwin.tar.gz"
-    sha256 "cc0e034e549d7fcf4e9bfa64a6b4ca83e4e49dc82cca199677210ea77ece0aaa"
-  end
+  # Currently only supports Apple Silicon Macs due to build constraints
+  depends_on arch: :arm64
 
   def install
     bin.install "rune" => "rune-vcs"
@@ -17,5 +15,6 @@ class RuneVcs < Formula
 
   test do
     system "#{bin}/rune-vcs", "--version"
+    assert_match "rune #{version}", shell_output("#{bin}/rune-vcs --version")
   end
 end
